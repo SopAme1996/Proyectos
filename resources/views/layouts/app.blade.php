8ad298c8fe7bc27e979dfca7433bb748a9d626a1
Luis Solis <sop199642@gmail.com>
2021-04-14 22:45:28 -0500

    Se cambio las rutas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
-    <script src="{{asset('js/menu_responsive.js')}}"></script>
-    <script src="{{asset('js/like_dislike.js')}}"></script>
+
+    <script src="./js/menu_responsive.js"></script>
+    <script src="./js/like_dislike.js"></script>
     <script src='https://kit.fontawesome.com/66f5e943e2.js' crossorigin='anonymous'></script>
 
 
@@ -30,9 +31,9 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/app_movil.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/util.css')}}">
+    <link rel="stylesheet" href="./css/app.css">
+    <link rel="stylesheet" href="./css/app_movil.css">
+    <link rel="stylesheet" href="./css/util.css">
 
 </head>
 
