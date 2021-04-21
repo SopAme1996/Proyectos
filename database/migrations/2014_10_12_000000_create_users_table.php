6a8ddfdc3a289e693630d074ca925824b688f4d1
Luis Solis <sop199642@gmail.com>
2021-04-21 13:55:28 -0500

    Se añadio mas columnas a la migracion de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('id');
+            $table->primary('id');
+            $table->string('role');
             $table->string('name');
+            $table->string('surname');
+            $table->string('nick');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->engine='InnoDB';
         });
     }
 
